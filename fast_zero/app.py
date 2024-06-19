46a395216d0861e747ba8447f146699c69a758f6
Carlos Eduardo <ducameneses16@gmail.com>
2024-06-18 21:38:28 -0300

    feat: Aula 02 - Adcionando o schema Message

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,8 +1,12 @@
+from http import HTTPStatus
+
 from fastapi import FastAPI
+
+from fast_zero.schemas import Message
 
 app = FastAPI(title='Curso Fast Zero')
 
 
-@app.get('/')
+@app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
     return {'message': 'Olá mundo'}
