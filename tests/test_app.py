f689ffa319da80b8f23773b843c95b9090426f96
Carlos Eduardo <ducameneses16@gmail.com>
2024-06-18 23:31:32 -0300

    feat: Exercicio Aula 02 - Adcionando um endpoint que retorna html e testa esse endpoint

--- tests/test_app.py
+++ tests/test_app.py
@@ -12,3 +12,23 @@ def test_root_deve_retorna_ok_e_ola_mundo():
 
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {'message': 'Olá mundo'}
+
+
+def test_index_deve_retorna_html_with_ola_mundo():
+    client = TestClient(app)
+
+    response = client.get('/index')
+
+    assert response.status_code == HTTPStatus.OK
+    assert (
+        response.text
+        == """
+    <html>
+      <head>
+        <title> Nosso olá mundo!</title>
+      </head>
+      <body>
+        <h1> Olá Mundo </h1>
+      </body>
+    </html>"""
+    )
